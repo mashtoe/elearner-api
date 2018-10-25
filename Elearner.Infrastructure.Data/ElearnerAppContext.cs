53ac1bd1a5ab1b4dce3da492d7d09b9653aa68c0
Rasmus Fjord Christensen <Fjord82@Rasmuss-MacBook-Pro-2.local>
2018-10-25 11:11:53 +0200

    added context class

---
+++ Elearner.Infrastructure.Data/ElearnerAppContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ELearner.Core.Entity;
+
+namespace Elearner.Infrastructure.Data
+{
+    public class ElearnerAppContext : DbContext
+    {
+        public ElearnerAppContext(DbContextOptions<ElearnerAppContext> options) : base (options){
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
