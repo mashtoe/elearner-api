3c462b3cc0c37b9d6c244348045aff4dffbbaa45
mashtoe <mashtoe@hotmail.com>
2018-10-25 14:43:54 +0200

    created coursecontroller and courseservice

---
+++ ELearner.Core/ApplicationService/ICourseService.cs
@@ -0,0 +1,21 @@
+﻿using ELearner.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELearner.Core.ApplicationService {
+    public interface ICourseService {
+        //returns new course
+        Course New();
+        //save one course (CRUD: Create)
+        Course Create(Course entity);
+        //get one course by id (CRUD: Read)
+        Course Get(int id);
+        //returns all courses (CRUD: Read)
+        List<Course> GetAll();
+        //Update  (CRUD: Update)
+        Course Update(Course entity);
+        //Delete one course with id (CRUD: Delete)
+        Course Delete(int id);
+    }
+}
---
+++ ELearner.Core/ApplicationService/Services/CourseService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ELearner.Core.DomainService;
+using ELearner.Core.Entity;
+
+namespace ELearner.Core.ApplicationService.Services {
+    public class CourseService : ICourseService {
+        readonly ICourseRepository _courseRepo;
+
+        public CourseService(ICourseRepository courseRepo) {
+            _courseRepo = courseRepo;
+        }
+        public Course New() {
+            return new Course();
+        }
+
+        public Course Create(Course entity) {
+            // TODO check if entity is valid, and throw errors if not
+            return _courseRepo.Create(entity);
+        }
+
+        public Course Delete(int id) {
+            return _courseRepo.Delete(id);
+        }
+
+        public Course Get(int id) {
+            return _courseRepo.Get(id);
+        }
+
+        public List<Course> GetAll() {
+            return _courseRepo.GetAll().ToList();
+        }
+
+        public Course Update(Course entity) {
+            return _courseRepo.Update(entity);
+        }
+    }
+}
