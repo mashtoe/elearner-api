6efe048f3134f188e81023b7fddc7fef9da2999d
mashtoe <mashtoe@hotmail.com>
2018-10-26 13:41:15 +0200

    many to many relation defined in dbcontext class

--- Elearner.Infrastructure.Data/ElearnerAppContext.cs
+++ Elearner.Infrastructure.Data/ElearnerAppContext.cs
@@ -10,6 +10,23 @@ namespace Elearner.Infrastructure.Data {
         public DbSet<Student> Students { get; set; }
         public DbSet<Course> Courses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
 
+
+            #region Many-Many Student Course
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(t => new { t.StudentId, t.CourseId });
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(stud => stud.Courses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(crs => crs.Students)
+                .HasForeignKey(sc => sc.StudentId);
+            #endregion  
+        }
     }
 }
