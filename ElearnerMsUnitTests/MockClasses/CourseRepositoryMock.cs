aba9c836792ad2c3c69c176f91c1298a3515d197
mashtoe <mashtoe@hotmail.com>
2018-12-20 11:53:36 +0100

    made a few tests for course service

---
+++ ElearnerMsUnitTests/MockClasses/CourseRepositoryMock.cs
@@ -0,0 +1,55 @@
+﻿using ELearner.Core.DomainService;
+using ELearner.Core.Entity.Entities;
+using ELearner.Core.Utilities.FilterStrategy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElearnerMsUnitTests.MockClasses {
+    public class CourseRepositoryMock : ICourseRepository {
+
+        public Course Create(Course course) {
+            return course;
+        }
+
+        public Course Delete(int id) {
+            return new Course() {
+                CategoryId = 1,
+                Category = new Category { Id = 1, Name = "Random course" },
+                Creator = new User() { Id = 1, Role = 0, Username = "MockMan" },
+                Id = id,
+                Name = "Mock Course",
+                CreatorId = 1,
+                Published = false,
+                Description = "Mock course description"
+            };
+        }
+
+        public Course Get(int id) {
+            return GetMockCourse();
+        }
+
+        public IEnumerable<Course> GetAll(List<IFilterStrategy> filters = null) {
+            var returnList = new List<Course>();
+            returnList.Add(GetMockCourse());
+            return returnList;
+        }
+
+        public IEnumerable<Course> GetAllById(IEnumerable<int> ids) {
+            throw new NotImplementedException();
+        }
+
+        private Course GetMockCourse() {
+            return new Course() {
+                CategoryId = 1,
+                Category = new Category { Id = 1, Name = "Random course" },
+                Creator = new User() { Id = 1, Role = 0, Username = "MockMan" },
+                Id = 1,
+                Name = "Mock Course",
+                CreatorId = 1,
+                Published = false,
+                Description = "Mock course description"
+            };
+        }
+    }
+}
---
+++ ElearnerMsUnitTests/MockClasses/UnitOfWorkMock.cs
@@ -0,0 +1,33 @@
+﻿using ELearner.Core.DomainService;
+using ELearner.Core.DomainService.UOW;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElearnerMsUnitTests.MockClasses {
+    public class UnitOfWorkMock : IUnitOfWork {
+
+        public IUserRepository UserRepo { get; }
+
+        public ICourseRepository CourseRepo { get; }
+
+        public ISectionRepository SectionRepo { get; }
+
+        public ILessonRepository LessonRepo { get; }
+
+        public ICategoryRepository CategoryRepo { get; }
+
+        public IApplicationRepository ApplicationRepo { get; }
+
+        public UnitOfWorkMock() {
+            CourseRepo = new CourseRepositoryMock();
+        }
+
+        public int Complete() {
+            return 0;
+        }
+
+        public void Dispose() {
+        }
+    }
+}
